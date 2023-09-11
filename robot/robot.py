a32a0474ffbfaaf8ff4d51edf733d9bcae7da872
KK <wkx611@outlook.com>
2023-09-11 21:10:20 +0800

    [add] kinematicchain - find node path logic

--- robot/robot.py
+++ robot/robot.py
@@ -222,7 +222,8 @@ class Node:
             parent_name is not None
         ), f"Parent name cannot be None, node {self.node_name} add parent failed."
         if self.parent is None:
-            self.parent = set().add(parent_name)
+            self.parent = set([])
+            self.parent.add(parent_name)
             return
         if parent_name in self.parent:
             warnings.warn(
@@ -235,7 +236,8 @@ class Node:
         if child_name is None:
             return
         if self.child is None:
-            self.child = set().add(child_name)
+            self.child = set([])
+            self.child.add(child_name)
             return
         if child_name in self.child:
             warnings.warn(
@@ -278,6 +280,8 @@ class KinematicChain:
     def __init__(self) -> None:
         self.nodes = {}
         self.base_name = "BASE"
+        self.base_node = Node(self.base_name, self.base_name, None)
+        self.nodes[self.base_name] = self.base_node
 
     @property
     def node_names(self):
@@ -292,18 +296,21 @@ class KinematicChain:
                     f"Node name {node_name} is not base name, add node to base instead."
                 )
             parent_name = self.base_name
+
         if node_name in self.node_names:
             warnings.warn(
                 f"Node name {node_name} already exist, add node failed, edit existing node instead."
             )
             self.edit_node_connection(node_name, parent_name, child_name)
             return
-        node = Node(node_name, parent_name, child_name)
-        self.nodes[node_name] = node
 
-    def _check_parent_name_exist(self, node_name: str) -> bool:
-        assert node_name is not None, "Parent name cannot be None."
-        return self._check_node_name_exist(node_name) or node_name == self.base_name
+        assert self._check_node_name_exist(parent_name), (
+            f"Parent name {parent_name} does not exist, " "add node failed."
+        )
+
+        node = Node(node_name, parent_name, child_name)
+        self.nodes[parent_name].add_child(node_name)
+        self.nodes[node_name] = node
 
     def _check_node_name_exist(self, node_name: str) -> bool:
         assert node_name is not None, "Node name cannot be None."
@@ -316,13 +323,19 @@ class KinematicChain:
         return None
 
     def add_parent_to_node(self, node_name: str, parent_name: str) -> None:
-        assert self._check_node_name_exist(
-            node_name
-        ), f"Node name {node_name} does not exist."
-        assert self._check_parent_name_exist(parent_name), (
+        if node_name == self.base_name:
+            warnings.warn(
+                f"Node name {node_name} is base name, cannot add parent to base."
+            )
+            return
+        assert self._check_node_name_exist(node_name), (
+            f"Node name {node_name} does not exist, " "add parent to node failed."
+        )
+        assert self._check_node_name_exist(parent_name), (
             f"Parent name {parent_name} does not exist, "
             f"add parent to node {node_name} failed."
         )
+
         node: Node = self.nodes[node_name]
         node.add_parent(parent_name)
 
@@ -338,6 +351,11 @@ class KinematicChain:
         node.add_child(child_name)
 
     def remove_parent_from_node(self, node_name: str, parent_name: str) -> None:
+        if node_name == self.base_name:
+            warnings.warn(
+                f"Node name {node_name} is base name, cannot remove parent from base."
+            )
+            return
         assert self._check_node_name_exist(
             node_name
         ), f"Node name {node_name} does not exist."
@@ -367,7 +385,7 @@ class KinematicChain:
         assert self._check_node_name_exist(
             node_name
         ), f"Node name {node_name} does not exist."
-        assert self._check_parent_name_exist(parent_name), (
+        assert self._check_node_name_exist(parent_name), (
             f"Parent name {parent_name} does not exist, "
             f"edit node {node_name} failed."
         )
@@ -387,23 +405,22 @@ class KinematicChain:
 
         if node.child is None:
             return [node_name]
-        # TODO: how to handle nest list
-        if len(node.child) == 1:
-            node_link = [node_name]
-            child_link = self.get_node_child_to_end(list(node.child)[0])
-            if isinstance(child_link[0], list):
-                for l in child_link:
-                    l.insert(0, node_name)
-                return child_link
-            assert isinstance(child_link[0], str)
-            node_link.extend(child_link)
-            return node_link
 
-        if len(node.child) > 1:
-            node_links = []
-            for ch in node.child:
-                node_links.append(self.get_node_child_to_end(ch))
-            return node_links
+        node_links = []
+        for ch in node.child:
+            child_links = self.get_node_child_to_end(ch)
+            assert len(child_links) > 0
+            if isinstance(child_links[0], list):
+                for l in child_links:
+                    l.insert(0, node_name)
+                    node_links.append(l)
+            else:
+                assert isinstance(child_links[0], str)
+                child_links.insert(0, node_name)
+                node_links.append(child_links)
+        if len(node_links) == 1:
+            return node_links[0]
+        return node_links
 
 
 class Robot2D(KinematicChain):
